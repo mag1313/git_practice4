e4aad2c59ca0dc6f879ec657c843e91d9bb520ce
Ievgenii Maglovanyi <maglovanyi13@gmail.com>
2022-01-24 10:47:44 -0500

    Hello2

--- src/hello/TestCase001.java
+++ src/hello/TestCase001.java
@@ -4,5 +4,6 @@ public class TestCase001 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("again");
+        System.out.println("Hello2");
     }
 }
